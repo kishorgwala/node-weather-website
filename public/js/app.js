fcffafec7a03af1c06ecf3a5a89e461ac1b7d6f0
kgwala <kgwala@mobiquityinc.com>
2020-06-17 19:20:04 +0530

    files changed to support harruko devployment

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) =>{
     msg1.innerHTML = ''
     msg2.innerHTML = ''
     console.log(search.value);
-    const url = 'http://localhost:3000/weather?address='+search.value
+    const url = '/weather?address='+search.value
     fetch(url).then((response) => {
     response.json().then( (data) => {
 
