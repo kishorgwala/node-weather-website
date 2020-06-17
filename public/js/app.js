2dcb9124427b12694ef9c73e7fed6166300878c2
kgwala <kgwala@mobiquityinc.com>
2020-06-17 19:02:01 +0530

    testing commite changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 console.log('Client side javascript file is loaded!')
+console.log('git test commitee')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
